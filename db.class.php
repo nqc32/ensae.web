3d6d89dc47d262290a578f32dfe10cd343460129
nqc32 <nqcuong32@gmail.com>
2014-06-27 01:52:23 +0200

    Change to mysqli

--- db.class.php
+++ db.class.php
@@ -12,16 +12,22 @@ class DB {
 		$this->db=cuong;
 		$this->user = root;
 		$this->pass = cuong;
-		$this->link = mysql_connect($this->host, $this->user, $this->pass);
-		mysql_query("SET NAMES utf8") ;
-		mysql_query("SET CHARACTER SET utf8");
-		mysql_select_db($this->db);
+		$this->link = mysqli_connect($this->host, $this->user, $this->pass,$this->db);
+		#mysqli_query($this->link,"SET NAMES utf8") ;
+		#mysqli_query($this->link;"SET CHARACTER SET utf8");
+		#mysqli_select_db($this->db);
+		if (!mysqli_set_charset($this->link, "utf8")) {
+		    #printf("Erreur lors du chargement du jeu de caractères utf8 : %s\n", mysqli_error($this->link));
+		} else {
+		    #printf("Jeu de caractères courant : %s\n", mysqli_character_set_name($this->link));
+		}
+		
 	}
    
 	function query($query) {
-		if (!$this->result = mysql_query($query, $this->link))  {
+		if ($this->result=mysqli_query($this->link,$query))  {
 			if ($this->debug) {
-				echo mysql_error()."<br />".$query;
+				echo mysqli_error()."<br />".$query;
 				echo "<hr />";
 			}
 		}
@@ -29,34 +35,34 @@ class DB {
 	}
 
 	function set_base($base) {
-		mysql_select_db($base);
+		mysqli_select_db($base);
 		return true;
 	}
    	
 	function fetch() {
-		$this->row=mysql_fetch_row($this->result);
+		$this->row=mysqli_fetch_row($this->result);
 		return $this->row;
 	}
       
 	function fetch_assoc() {
-		$this->row=mysql_fetch_assoc($this->result);
+		$this->row=mysqli_fetch_assoc($this->result);
 		return $this->row;
 	}
    
 	function field_name($field_offset) {
-		return mysql_field_name($this->result,$field_offset);
+		return mysqli_field_name($this->result,$field_offset);
 	}
    
 	function num_fields() {
-		return mysql_num_fields($this->result);
+		return mysqli_num_fields($this->result);
 	}
 	
 	function insert_id() {
-		return mysql_insert_id();
+		return mysqli_insert_id();
 	}
 	
 	function close() {
-		mysql_close($this->link);
+		mysqli_close($this->link);
 	}   
 }
 /**
